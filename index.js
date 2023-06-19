e59ea4f54a47f23c8fe54a17e7474f87cab24bcc
deru1292 <123286743+deru1292@users.noreply.github.com>
2023-06-18 20:23:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(){
+    console.log ("This Saturday, I wannt to ...!")
+    return "This Saturday, I want to roller-skate!"
+    
+}
+
+
+function mondayWork() { 
+     
+    return "This Monday, I will work from home." 
+}
+
+function wrapAdjective() {
+    return "You are *a hard worker*!!!"
+
+}
